a0236b2ebca17776fcce1c7d4a4c2e5153099150
Reegan Layzell <reegan.layzell@live.co.uk>
2015-07-02 17:54:19 +0100

    Added DeviceType and MakerCode enumerations that describe the type of device that a ROM is built for (DS/DSi/Both) and the Maker of the ROM/Game

---
+++ Nitro.IO/DeviceType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitro.IO
+{
+	/// <summary>
+	/// Specifies the type of Device/Platform a particular
+	/// game has been developed for.
+	/// </summary>
+	[Flags]
+	public enum DeviceType
+	{
+		/// <summary>
+		/// The Nintendo DS Platform.
+		/// </summary>
+		NintendoDS = 0x01,
+
+
+		/// <summary>
+		/// The Nintendo DSi Platform.
+		/// </summary>
+		NintendoDSi = 0x02,
+
+
+		/// <summary>
+		/// Both the Nintendo DS and DSi Platforms.
+		/// </summary>
+		NintendoDSAndDSi = NintendoDS | NintendoDSi,
+	}
+}
---
+++ Nitro.IO/MakerCode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitro.IO
+{
+	public enum MakerCode : ushort
+	{
+		Unknown = 0xFFFF,
+		Homebrew = 0x00,
+		Nintendo = 0x01,
+	}
+
+	internal static class MakerCodes
+	{
+		private static readonly Dictionary<byte[], MakerCode> _makeCodes = new Dictionary<byte[], MakerCode>( ) {
+			{ Encoding.ASCII.GetBytes("00"), MakerCode.Homebrew },
+			{ Encoding.ASCII.GetBytes("01"), MakerCode.Nintendo },
+		};
+
+		public static MakerCode FromBytes(byte[] makerCode, int startIndex = 0)
+		{
+			byte[] tmp = new byte[] { makerCode[startIndex], makerCode[startIndex + 1] };
+			var result = _makeCodes.SingleOrDefault(x => Enumerable.SequenceEqual(tmp, x.Key));
+
+			if (result.Equals(default(KeyValuePair<byte[], MakerCode>)))
+				return MakerCode.Unknown;
+
+			return result.Value;
+		}
+	}
+}
