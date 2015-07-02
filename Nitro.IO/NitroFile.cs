286e7370a02f6ee16427a093285957311da364bf
Reegan Layzell <reegan.layzell@live.co.uk>
2015-07-02 17:55:07 +0100

    Added NitroFile static class intended to be used in a similar manner to System.IO.File

---
+++ Nitro.IO/NitroFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nitro.IO
+{
+	public static class NitroFile
+	{
+		internal struct FntMainTable
+		{
+			public UInt32 SubtableOffset;
+			public UInt16 FirstFileId;
+			public UInt16 DirectoryCount;
+			public UInt16 ParentId;
+		}
+
+		internal struct FntSubTable
+		{
+			byte TypeOrLength;
+			string Name;
+		}
+
+		public static bool Exists(Rom rom, string path)
+		{
+			throw new NotImplementedException( );
+		}
+
+		public static FileStream Open(Rom rom, string path)
+		{
+			throw new NotImplementedException( );
+		}
+	}
+}
